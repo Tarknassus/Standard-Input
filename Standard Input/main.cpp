58f57278373aa8f623cda633c8ecc573ca7cf02d
The-Bearded-Vegan <63019033+The-Bearded-Vegan@users.noreply.github.com>
2020-04-02 08:04:06 +0100

    Main program written

--- Standard Input/main.cpp
+++ Standard Input/main.cpp
@@ -8,8 +8,12 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main ()
+{
+    int i;
+    std::cout << "Please enter an integer value: ";
+    std::cin >> i;
+    std::cout << "The value you entered is " << i;
+    std::cout << " and its double is " << i*2 << ".\n";
     return 0;
 }
