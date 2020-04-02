8996c9a2b0ac820287c208a703c56329b07358c2
The-Bearded-Vegan <63019033+The-Bearded-Vegan@users.noreply.github.com>
2020-04-02 08:08:14 +0100

    Make it sound more human.

--- Standard Input/main.cpp
+++ Standard Input/main.cpp
@@ -11,7 +11,7 @@
 int main ()
 {
     int i;
-    std::cout << "Please enter an integer value: ";
+    std::cout << "Please enter a numerical value: ";
     std::cin >> i;
     std::cout << "The value you entered is " << i;
     std::cout << " and its double is " << i*2 << ".\n";
